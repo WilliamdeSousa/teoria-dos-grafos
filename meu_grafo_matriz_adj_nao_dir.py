f8a5aa08af21409cc3bd28248d66a8f86893619b
William de Sousa Costa <37248765+WilliamdeSousa@users.noreply.github.com>
2024-12-17 23:11:05 -0300

    ✅ : Roteiro 4
    - implementar funções do roteiro 1 com matrizes de adjacências não direcionada

--- meu_grafo_matriz_adj_nao_dir.py
+++ meu_grafo_matriz_adj_nao_dir.py
@@ -11,14 +11,24 @@ class MeuGrafo(GrafoMatrizAdjacenciaNaoDirecionado):
         Onde X, Z e W são vértices no grafo que não tem uma aresta entre eles.
         :return: Um conjunto (set) com os pares de vértices não adjacentes
         '''
-        pass
+        vertices_nao_adj = set()
+
+        for v in range(len(self.vertices)):
+            for w in range(v + 1, len(self.vertices)):
+                if len(self.matriz[v][w]) == 0:
+                    vertices_nao_adj.add(f'{self.vertices[v].rotulo}-{self.vertices[w].rotulo}')
+        return vertices_nao_adj
+                    
 
     def ha_laco(self):
         '''
         Verifica se existe algum laço no grafo.
         :return: Um valor booleano que indica se existe algum laço.
         '''
-        pass
+        for i in range(len(self.vertices)):
+            if len(self.matriz[i][i]) > 0:
+                return True
+        return False
 
 
     def grau(self, V=''):
@@ -28,14 +38,28 @@ class MeuGrafo(GrafoMatrizAdjacenciaNaoDirecionado):
         :return: Um valor inteiro que indica o grau do vértice
         :raises: VerticeInvalidoError se o vértice não existe no grafo
         '''
-        pass
+        if not self.existe_rotulo_vertice(V):
+            raise VerticeInvalidoError
+        
+        soma = 0
+        index_de_v = self.indice_do_vertice(self.get_vertice(V))
+        linha = self.matriz[index_de_v]
+        for i in range(len(self.vertices)):
+            soma += len(linha[i])
+            if index_de_v == i:
+                soma += len(linha[i])
+        return soma
 
     def ha_paralelas(self):
         '''
         Verifica se há arestas paralelas no grafo
         :return: Um valor booleano que indica se existem arestas paralelas no grafo.
         '''
-        pass
+        for v in range(len(self.vertices)):
+            for w in range(len(self.vertices)):
+                if len(self.matriz[v][w]) > 1:
+                    return True
+        return False
 
     def arestas_sobre_vertice(self, V):
         '''
@@ -45,11 +69,22 @@ class MeuGrafo(GrafoMatrizAdjacenciaNaoDirecionado):
         :return: Um conjunto com os rótulos das arestas que incidem sobre o vértice
         :raises: VerticeInvalidoError se o vértice não existe no grafo
         '''
-        pass
+        if not self.existe_rotulo_vertice(V):
+            raise VerticeInvalidoError
+        arestas = set()
+        index = self.indice_do_vertice(self.get_vertice(V))
+        for v in range(len(self.vertices)):
+            for key in self.matriz[v][index]:
+                arestas.add(key)
+        return arestas
 
     def eh_completo(self):
         '''
         Verifica se o grafo é completo.
         :return: Um valor booleano que indica se o grafo é completo
         '''
-        pass
+        for v in range(len(self.vertices)):
+            for u in range(len(self.vertices)):
+                if (u == v and len(self.matriz[v][u]) != 0) or (u != v and len(self.matriz[v][u]) != 1):
+                    return False
+        return True
--- teste.py
+++ teste.py
@@ -1,1 +1,9 @@
-from bibgrafo.grafo_lista_adj_nao_dir import GrafoListaAdjacenciaNaoDirecionado
+from meu_grafo_lista_adj_nao_dir import MeuGrafo
+from bibgrafo.grafo_json import GrafoJSON
+
+grafo = GrafoJSON.json_to_grafo('test_json/grafo_l3.json', MeuGrafo())
+
+print(grafo)
+for i in range(1, 8):
+    print(f'caminho de tamanho {i}: {grafo.caminho(i)}')
+
